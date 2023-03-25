85c836382f27f7252b1efff1549a574a16f69860
Matt Brockman <mattbrockman@gmail.com>
2023-03-25 13:53:24 -0700

    autoscrolls

--- src/components/HistoryDisplay.js
+++ src/components/HistoryDisplay.js
@@ -1,13 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { StateContext } from "../contexts/StateContext";
 
 import styled from "@emotion/styled";
 
 export const HistoryDisplay = () => {
   const { history } = useContext(StateContext);
+  const historyRef = useRef(null);
+
+  const historyLength = history.length;
+  useEffect(() => {
+    if (historyRef.current) {
+      historyRef.current.scrollTop = historyRef.current.scrollHeight;
+    }
+  }, [historyLength]);
 
   return (
-    <HistoryContainer>
+    <HistoryContainer ref={historyRef}>
       {history.map((message) => {
         return <HistoryMessage>{JSON.stringify(message)}</HistoryMessage>;
       })}
@@ -17,7 +25,7 @@ export const HistoryDisplay = () => {
 
 const HistoryContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   //give it a border with box-shadow
   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.5);
   width: 100%;
@@ -29,4 +37,11 @@ const HistoryContainer = styled.div`
 
 const HistoryMessage = styled.div`
   white-space: pre-wrap;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 10px;
+  background-color: #f0f0f0;
+  color: #000000;
+  border: 1px solid #000000;
+  width: calc(100% - 20px);
 `;
